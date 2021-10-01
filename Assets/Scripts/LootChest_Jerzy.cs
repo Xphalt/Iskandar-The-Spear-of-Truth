f569a9812f827245197b71541792bc3e4435eeec
rzed1 <45627650+rzed1@users.noreply.github.com>
2021-10-01 20:02:25 +0100

    Added loot chests with interaction

---
+++ Assets/Scripts/LootChest_Jerzy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootChest_Jerzy : MonoBehaviour
+{
+
+    public bool isInteractable;
+    public GameObject lootChestLidPivot;
+    public int minGems;
+    public int maxGems;
+    public GameObject GemPrefab;
+    public float gemHeightOffset;
+
+    public float lootHeightForce;
+    public float lootForwardForce;
+    public float lootSideForce;
+
+    const float LOWER_RANDOM_ANGLE_BOUNDARY = 0.5f;
+    const float UPPER_RANDOM_ANGLE_BOUNDARY = 0.7f;
+
+
+    void Start()
+    {
+        isInteractable = true;
+    }
+
+
+    public void Interact()
+    {
+        lootChestLidPivot.GetComponent<Animator>().Play("LootChestLidOpening");
+
+        //generate loot (currently only gems)
+        int amountOfGems = Random.Range(minGems, maxGems);
+
+        //display loot
+        for (int gems = 0; gems < amountOfGems; gems++)
+        {
+
+            GameObject obj = Instantiate(GemPrefab, transform.position + new Vector3(0, gemHeightOffset, 0), transform.rotation);
+            obj.GetComponent<Rigidbody>().AddForce(transform.up * Random.Range(LOWER_RANDOM_ANGLE_BOUNDARY, UPPER_RANDOM_ANGLE_BOUNDARY) * lootHeightForce);
+            obj.GetComponent<Rigidbody>().AddForce(transform.forward * Random.Range(LOWER_RANDOM_ANGLE_BOUNDARY, UPPER_RANDOM_ANGLE_BOUNDARY) * lootForwardForce);
+            obj.GetComponent<Rigidbody>().AddForce(transform.right * Random.Range(-LOWER_RANDOM_ANGLE_BOUNDARY, UPPER_RANDOM_ANGLE_BOUNDARY) * lootSideForce);
+
+
+        }
+
+
+        isInteractable = false;
+    }
+}
