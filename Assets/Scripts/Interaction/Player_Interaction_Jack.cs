c3fd8f71fa316a6006030628f519df7b84872885
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-10-04 11:52:10 +0100

    # Fixed interaction merge
    Co-Authored-By: JBodicoat <36885653+JBodicoat@users.noreply.github.com>
    Co-Authored-By: Lewis Cozens <78376308+LeCoz96@users.noreply.github.com>
    Co-Authored-By: Tiago Antunes <55832959+Sygris@users.noreply.github.com>

--- Assets/Scripts/Interaction/Player_Interaction_Jack.cs
+++ Assets/Scripts/Interaction/Player_Interaction_Jack.cs
@@ -8,7 +8,7 @@ public class Player_Interaction_Jack : MonoBehaviour
     [SerializeField]
     private float _interactionRadius = 5.0f;
 
-    private const int INTERACTABLE_LAYERMASK = 1 << 6;
+    private const int INTERACTABLE_LAYERMASK = 1 << 8;
 
     private Collider[] _interactableColliders;
 
