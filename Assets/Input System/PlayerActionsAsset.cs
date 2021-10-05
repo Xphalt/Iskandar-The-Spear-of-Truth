1a1c1d4df0e1e683872c07b98f474368fab94462
Tiago Antunes <tiago.a.vista@gmail.com>
2021-10-05 15:18:22 +0100

    My last attempt before killing myself

---
+++ Assets/Input System/PlayerActionsAsset.cs
@@ -0,0 +1,324 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input System/PlayerActionsAsset.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerActionsAsset : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerActionsAsset()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerActionsAsset"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""4eb4e2c1-5726-433a-ac27-7bb5151f887b"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""4f02be9e-6021-4ea6-bfbe-6e43f250b61a"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""b6e5aaa1-4dff-478b-a7e1-5afdb6b2db10"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a09c1073-6ac1-4ef6-b90b-32881d30075d"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""71ff6817-497a-4978-8835-094d9b3e5249"",
+                    ""path"": ""<Keyboard>/p"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""19ec4f23-ea2b-409d-ac4c-8a190c0d8fe9"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c5e4ecc0-1de5-4cae-ae68-82220f148bfc"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""b5a78475-5184-487f-beb6-cacbce53d766"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ab7ef90a-cc92-4585-a85a-833a786029f9"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""cda2b6b0-4579-46bd-99fb-0763a9df1c23"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        },
+        {
+            ""name"": ""UI"",
+            ""id"": ""2f73ad22-2049-4ed8-9e4f-6a4ed3486ecc"",
+            ""actions"": [
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""0773660c-be92-46ff-b40b-e2db90f4aae0"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a0eda673-76e1-4132-a8b3-71c7b4a2bcaa"",
+                    ""path"": ""<Keyboard>/p"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Pause = m_Player.FindAction("Pause", throwIfNotFound: true);
+        // UI
+        m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
+        m_UI_Pause = m_UI.FindAction("Pause", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Pause;
+    public struct PlayerActions
+    {
+        private @PlayerActionsAsset m_Wrapper;
+        public PlayerActions(@PlayerActionsAsset wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Pause => m_Wrapper.m_Player_Pause;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Pause.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPause;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+
+    // UI
+    private readonly InputActionMap m_UI;
+    private IUIActions m_UIActionsCallbackInterface;
+    private readonly InputAction m_UI_Pause;
+    public struct UIActions
+    {
+        private @PlayerActionsAsset m_Wrapper;
+        public UIActions(@PlayerActionsAsset wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Pause => m_Wrapper.m_UI_Pause;
+        public InputActionMap Get() { return m_Wrapper.m_UI; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(UIActions set) { return set.Get(); }
+        public void SetCallbacks(IUIActions instance)
+        {
+            if (m_Wrapper.m_UIActionsCallbackInterface != null)
+            {
+                @Pause.started -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+            }
+            m_Wrapper.m_UIActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
+            }
+        }
+    }
+    public UIActions @UI => new UIActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnPause(InputAction.CallbackContext context);
+    }
+    public interface IUIActions
+    {
+        void OnPause(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Entities/Player/PlayerInput.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Reference Variables
+    private PlayerActionsAsset _playerActionsAsset;
+    private Rigidbody _playerRigidbody;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float _movementSpeed;
+
+    [Header("Dash Settings")]
+    [SerializeField] private float _dashDistance;
+    [SerializeField] private float _dashSpeed;
+    [SerializeField] private float _dashCooldown;
+    [SerializeField] private int _invulnerableFrames;
+
+    private void Awake()
+    {
+        _playerActionsAsset = new PlayerActionsAsset();
+        _playerRigidbody = GetComponent<Rigidbody>();
+
+        _playerActionsAsset.Player.Pause.performed += OnPause;
+        //_playerActionsAsset.Player.Dash.performed += OnDash;
+
+        _playerActionsAsset.UI.Pause.performed += OnPause;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = _playerActionsAsset.Player.Movement.ReadValue<Vector2>();
+        Debug.Log(inputVector);
+        _playerRigidbody.AddForce(new Vector3(inputVector.x, 0.0f, inputVector.y) * _movementSpeed, ForceMode.Force);
+    }
+
+    // Maybe OnDash() Function should call the Dash Function from some other script to keep it clean
+    #region Dash (IN PROGRESS)
+    //private void OnDash(InputAction.CallbackContext ctx)
+    //{
+    //    Vector2 inputVector = _playerActionsAsset.Player.Movement.ReadValue<Vector2>();
+
+    //    if (inputVector != Vector2.zero)
+    //    {
+    //        StartCoroutine(Dash());
+    //    }
+    //}
+
+    //IEnumerator Dash()
+    //{
+    //    Vector2 inputVector = _playerActionsAsset.Player.Movement.ReadValue<Vector2>();
+
+    //    _playerRigidbody.AddForce(new Vector3(inputVector.x, 0.0f, inputVector.y) * _dashSpeed, ForceMode.Force);
+
+    //    yield return new WaitForSeconds(_dashCooldown);
+    //} 
+    #endregion
+
+    private void OnPause(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Previous Action Map: " + ctx.action.actionMap.name);
+
+        switch (ctx.action.actionMap.name)
+        {
+            case "Player":
+                _playerActionsAsset.Player.Disable();
+                _playerActionsAsset.UI.Enable();
+                // Call Pause Function
+                break;
+            case "UI":
+                _playerActionsAsset.UI.Disable();
+                _playerActionsAsset.Player.Enable();
+                // Call Pause/Unpause Function
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _playerActionsAsset.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerActionsAsset.Player.Disable();
+    }
+}
