3bbef1926a1684bbd61f80634c27fae87654c514
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2021-10-07 18:59:04 +0100

    Added enemy health bar in LockOnUX scene - code in UIManager to setup and update

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+// Dominique 07-10-2021, Allow us to give the enemy health bar a name
+using TMPro;
+
+#if DEBUG // Dominique 07-10-2021, Needed for debugging
+using System.Collections;
+#endif // DEBUG
 
 /*
  * Created by Mattie Hilton - 03/10/2021 
@@ -16,8 +22,8 @@ public class UIManager : MonoBehaviour
     [Tooltip("Health Bar Slider Parent Prefab")]
     public GameObject HealthBarUI;
 
-    [Header("This is set to be this value times 3, to keep the UI neat. E.G. 1x3 health")]
-    public int maxHealth = 1;
+    [Header("How many segments each heart has - 1 damage removes 1 segment")]
+    public int heartSegments = 1;
 
     [ContextMenu("Use a postive or negative Int, to affect the health on the next fixed frame")]
     public void UpdateHealthBar(int healthChange)
@@ -26,6 +32,45 @@ public class UIManager : MonoBehaviour
     }
     #endregion
 
+    // Dominique 07-10-2021, We can now setup the health bar for a boss with their name
+    // Show an addition/loss of health with UpdateEnemyHealthBar(value)
+    #region Enemy Health Bar
+    public GameObject enemyHealthBarUI;
+    private TextMeshProUGUI enemyNameText;
+    private Slider enemyHealthSlider;
+
+    public void SetupEnemyHealthBar(string enemyName, int enemyMaxHealth)
+    {
+        enemyNameText.text = enemyName;
+        enemyHealthSlider.maxValue = enemyMaxHealth;
+        enemyHealthSlider.value = enemyMaxHealth;
+
+        ToggleEnemyHealthBar(true);
+    }
+    public void UpdateEnemyHealthBar(int healthChange)
+    {
+        // Make sure we don't make the slider value -ve
+        if (enemyHealthSlider.value + healthChange < enemyHealthSlider.minValue)
+        {
+            enemyHealthSlider.value = enemyHealthSlider.minValue;
+        }
+        // Make sure slider doesn't go above max value
+        else if (enemyHealthSlider.value + healthChange > enemyHealthSlider.maxValue)
+        {
+            enemyHealthSlider.value = enemyHealthSlider.maxValue;
+        }
+        else
+        {
+            enemyHealthSlider.value += healthChange;
+        }
+    }
+    // So we can hide the health bar when the enemy dies
+    public void ToggleEnemyHealthBar(bool visible)
+    {
+        enemyHealthBarUI.SetActive(visible);
+    }
+
+    #endregion
 
     public static UIManager instance;
     private void Awake()
@@ -35,7 +80,20 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        HealthBarUI.transform.GetChild(0).GetComponent<Slider>().maxValue = maxHealth*3;
+        HealthBarUI.transform.GetChild(0).GetComponent<Slider>().maxValue = heartSegments * 3;
+
+        enemyHealthSlider = enemyHealthBarUI.GetComponentInChildren<Slider>();
+        enemyNameText = enemyHealthBarUI.GetComponentInChildren<TextMeshProUGUI>();
     }
 
+#if DEBUG // Dominique 07-10-2021, Use to test enemy health bar (make sure to SetupEnemyHealthBar first)
+    IEnumerator TestEnemyBar()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            UpdateEnemyHealthBar(-100);
+            yield return new WaitForSeconds(1);
+        }
+    }
+#endif // DEBUG
 }
