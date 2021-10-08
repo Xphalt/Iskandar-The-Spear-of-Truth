aa50ac032119b3bcfd74c4a76b2996500bde8d78
VoidWolf6 <78429550+VoidWolf6@users.noreply.github.com>
2021-10-08 16:42:31 +0100

    It works at last. 'Nuff said.

--- Assets/Scripts/Entities/Player/CameraMove.cs
+++ Assets/Scripts/Entities/Player/CameraMove.cs
@@ -10,29 +10,23 @@ public class CameraMove : MonoBehaviour
     public Transform RightBound;
     public Transform Target;
 
-    public bool centred = true; 
     private bool XFollowing, ZFollowing;
+    public float Zoffset;
 
     private Camera thisCamera;
 
-    public List<Transform> midGrounds;
-    public List<float> midGroundRatios;
-
-    private Vector3 Limits;   
-    public List<Vector3> midGroundStarts;
+    public Vector3 Limits;   
 
 
-
-    private void Awake()
+    private void Awake() //used to avoid reference errors
     {
-        thisCamera = GetComponent<Camera>();
-        Limits = thisCamera.ScreenToWorldPoint(new Vector3(Screen.width, thisCamera.transform.position.y, Screen.height));
-        if (centred)    Limits -= transform.position;
-        foreach (Transform mg in midGrounds)    midGroundStarts.Add(mg.position);
+        thisCamera = GetComponent<Camera>();       
+        //Limits = new Vector3(0, thisCamera.transform.position.y, 0);
     }
 
     void Update()
     {
+        
         XFollowing = RightBound.position.x - LeftBound.position.x > Limits.x * 2;
         ZFollowing = UpBound.position.z - DownBound.position.z > Limits.z * 2;
 
@@ -42,13 +36,8 @@ public class CameraMove : MonoBehaviour
         float newX = XFollowing ? Mathf.Clamp(Target.position.x, LeftBound.position.x + Limits.x, RightBound.position.x - Limits.x) : Xmid;
         float newZ = ZFollowing ? Mathf.Clamp(Target.position.z, DownBound.position.z + Limits.z, UpBound.position.z - Limits.z) : Zmid;
 
-        transform.position = new Vector3(newX, transform.position.y, newZ);
+        transform.position = new Vector3(newX, transform.position.y, (newZ - Zoffset));
 
-        for (int m = 0; m < midGrounds.Count; m++)
-        {
-            Vector3 newMidGround = Vector3.Lerp(midGroundStarts[m], transform.position, midGroundRatios[m]);
-            midGrounds[m].position = new Vector3(newMidGround.x, midGrounds[m].position.y, newMidGround.z);
-        }
     }
 
     
