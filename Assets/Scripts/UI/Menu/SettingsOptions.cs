a5f826349343ce9fc120ff7c16409961abeba9f6
whatever9999 <30952031+whatever9999@users.noreply.github.com>
2021-10-08 11:15:27 +0100

    Update quality settings options according to language

--- Assets/Scripts/UI/Menu/SettingsOptions.cs
+++ Assets/Scripts/UI/Menu/SettingsOptions.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
 
 /*
  * Created by Mattie Hilton - 03/10/2021 
@@ -8,17 +9,45 @@ using System.Collections.Generic;
 public class SettingsOptions : MonoBehaviour
 {
     public Dropdown qualityDropDown;
+    public string qualitySettingsStringTable = "QualitySettings";
 
-    void Start()
+    private LocaleIdentifier current_locale;
+    private bool done_first_setup = false;
+
+    // Dominique 08-10-2021, When the pause button is pressed the pause menu opens and the quality settings are populated with options taken from the current locale
+    public void LocaleChange()
     {
-        qualityDropDown.ClearOptions();
+        LocaleIdentifier new_locale = LocalizationSettings.SelectedLocale.Identifier;
 
-        foreach (var name in QualitySettings.names)
+        if (!done_first_setup || (new_locale != current_locale))
         {
-            qualityDropDown.options.Add(new Dropdown.OptionData(name));
-        }
+            qualityDropDown.ClearOptions();
 
-        SetQuality(QualitySettings.GetQualityLevel());
+            if (new_locale.Code.Equals("en"))
+            {
+                foreach (var name in QualitySettings.names)
+                {
+                    qualityDropDown.options.Add(new Dropdown.OptionData(name));
+                }
+            }
+            // Use the English quality names to get the codes from the French part of the QualitySettings table
+            else if (new_locale.Code.Equals("fr"))
+            {
+                LocalizedString localisedString = new LocalizedString();
+                localisedString.TableReference = qualitySettingsStringTable;
+                foreach (var name in QualitySettings.names)
+                {
+                    localisedString.TableEntryReference = name;
+                    string french_name = localisedString.GetLocalizedString();
+                    qualityDropDown.options.Add(new Dropdown.OptionData(french_name));
+                }
+            }
+
+            SetQuality(QualitySettings.GetQualityLevel());
+
+            current_locale = new_locale;
+            if (!done_first_setup) done_first_setup = true;
+        }
     }
 
     public void SetQuality(int qualityIndex)
