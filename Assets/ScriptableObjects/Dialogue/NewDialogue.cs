d7b3b210353c9e1da58d3c7760102d2aec078982
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2021-10-15 16:52:26 +0100

    Changed how the dialogue scriptable objects work
    It should now be easier to use them within cutscenes and conversations between NPC's and the player.

--- Assets/ScriptableObjects/Dialogue/NewDialogue.cs
+++ Assets/ScriptableObjects/Dialogue/NewDialogue.cs
@@ -1,11 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Dialogue/NewDialogue", fileName = "NewDialogue")]
 [SerializeField]
 public class NewDialogue : ScriptableObject
 {
-    public string NPCName;
+    [System.Serializable]
+    public struct NextSentence
+    {
+        public string NPCName;
 
-    [TextArea]
-    public string[] NumOfSentences;
+        [TextArea]
+        public string[] NumOfSentences;
+    }
+
+    public List<NextSentence> ListOfNewSentences = new List<NextSentence>();
 }
