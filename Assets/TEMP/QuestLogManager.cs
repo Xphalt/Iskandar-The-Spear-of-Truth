3104c8775a3a268248fdbd239fe9995ffa9459e6
Linden Woodford <41804445+Xphalt@users.noreply.github.com>
2021-10-16 21:40:18 +0100

    Fixed questlog

--- Assets/TEMP/QuestLogManager.cs
+++ Assets/TEMP/QuestLogManager.cs
@@ -5,33 +5,26 @@ using UnityEngine.UI;
 public class QuestLogManager : MonoBehaviour
 {
     public GameObject ButtonArea;
-    public GameObject QuestButton;
+    public Button QuestButton;
 
     //Add quests to list
     public List<QuestObject> ListOfQuests = new List<QuestObject>();
-    private List<GameObject> ListOfButtons = new List<GameObject>();
+    //[HideInInspector]
+    public List<Button> ListOfButtons = new List<Button>();
+    public Dictionary<QuestObject, Button> QuestDictionary = new Dictionary<QuestObject, Button>();
 
-    //Create an buttons equal to amount of quests
-    public void Awake()
+    private void Awake()
     {
         foreach (var item in ListOfQuests)
         {
             ListOfButtons.Add(QuestButton);
+            QuestButton.GetComponentInChildren<Text>().text = item.QuestName;
+            Instantiate(QuestButton, ButtonArea.transform).name = item.QuestName;
         }
 
-        foreach (var item in ListOfButtons)
+        for (int i = 0; i < ListOfQuests.Count; i++)
         {
-            Instantiate(QuestButton, ButtonArea.transform);
+            QuestDictionary.Add(ListOfQuests[i], ListOfButtons[i]);
         }
     }
-
-    public void Update()
-    {
-            ListOfButtons[0].transform.GetChild(0).GetComponentInChildren<Text>().text.Equals(ListOfQuests[0].name.ToString());
-    }
-
-    //Link quests to buttons
-    //Make way of specifying certain quests
-    //Change button text to quest name
-    //Change quest description when quest is clicked
 }
