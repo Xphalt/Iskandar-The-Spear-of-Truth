005780c883190c4d4747748ea899b0f349266eb2
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2021-10-18 19:33:03 +0100

    + Enemies added with scripts

---
+++ Assets/Scripts/UI/Dialogue/IronworkerCollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronworkerCollision : MonoBehaviour
+{
+    public GameObject Orc1;
+    public GameObject Orc2;
+    public GameObject Orc3;
+
+    public GameObject NearBoss;
+    public GameObject FarBoss;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Pause time
+        // Conversation with ironworker
+        // Trigger enemies to spawn
+    }
+}
---
+++ Assets/Scripts/UI/Dialogue/MotherCollision.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Pause time
+        // Conversation with mum
+    }
+}
