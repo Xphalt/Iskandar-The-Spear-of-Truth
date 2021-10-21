0d0e0bf0dde014d4528465e0e04e2a962529b7ce
Zin227 <bernardoreis2000@hotmail.com>
2021-10-21 18:40:52 +0100

    #Fixed FOVGizmo errors

--- Assets/Editor/FOVEditorGizmo.cs
+++ Assets/Editor/FOVEditorGizmo.cs
@@ -1,28 +1,24 @@
-﻿////Bernardo Mendes
-////AI Programming Dept.
-////Latest Rev: 29/09/2021
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEditor;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 
-//[CustomEditor(typeof(PlayerDetection))]
-//public class FOVEditorGizmo : Editor
-//{
+[CustomEditor(typeof(PlayerDetection))]
+public class FOVEditorGizmo : Editor
+{
 
-//    private void OnSceneGUI()
-//    {
-//        PlayerDetection detection = (PlayerDetection)target;
-//        Handles.color = Color.green;
-//        Handles.DrawWireArc(detection.transform.position, Vector3.up, Vector3.forward, 360, detection.detectionRadius);
-//        Vector3 viewAngleA = detection.DirFromAngle(-detection.viewAngle / 2, false);
-//        Vector3 viewAngleB = detection.DirFromAngle(detection.viewAngle / 2, false);
+    private void OnSceneGUI()
+    {
+        PlayerDetection detection = (PlayerDetection)target;
+        Handles.color = Color.green;
+        Handles.DrawWireArc(detection.transform.position, Vector3.up, Vector3.forward, 360, detection.detectionRadius);
+        Vector3 viewAngleA = detection.DirFromAngle(-detection.viewAngle / 2, false);
+        Vector3 viewAngleB = detection.DirFromAngle(detection.viewAngle / 2, false);
 
-//        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleA * detection.detectionRadius);
-//        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleB * detection.detectionRadius);
+        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleA * detection.detectionRadius);
+        Handles.DrawLine(detection.transform.position, detection.transform.position + viewAngleB * detection.detectionRadius);
 
-//        Handles.color = Color.yellow;
-//        Handles.DrawLine(detection.transform.position, detection.GetCurTarget().position);
+        Handles.color = Color.yellow;
     
-//    }
-//}
+    }
+}
