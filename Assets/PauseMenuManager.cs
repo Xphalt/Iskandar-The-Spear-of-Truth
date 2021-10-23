c5ca229df73c9dcf96338b791a615bf3c0d9e45d
Mattie Hilton <78374721+SnarfM@users.noreply.github.com>
2021-10-23 16:36:53 +0100

    Added Panning left and right to menus
    I also added background to the menu and interpolated pause menu times

--- Assets/PauseMenuManager.cs
+++ Assets/PauseMenuManager.cs
@@ -10,6 +10,8 @@ public class PauseMenuManager : MonoBehaviour
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
 
+    public Animator anim;
+
     public TextMeshProUGUI leftOption;
     public TextMeshProUGUI rightOption;
     public TextMeshProUGUI title;
@@ -22,26 +24,107 @@ public class PauseMenuManager : MonoBehaviour
 
     private int currentPage = 0;
 
+    public float LerpPauseSpeed = 0.1f;
+
     private void Start()
     {
         Resume();
     }
 
+    private void FixedUpdate()
+    {
+        if (gameIsPaused && Time.timeScale > 0)
+        {
+            //Time.timeScale -=LerpPauseSpeed; //To Lerp
+            Time.timeScale = 0f; //To have no Lerp
+        }
+        else if (!gameIsPaused && Time.timeScale < 1)
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 1, LerpPauseSpeed);
+
+            if (Time.timeScale > 0.98f)
+                Time.timeScale = 1f;
+        }
+    }
+
     public void UpdatePauseUI(int pageTurn)
     {
-        equipmentMenu.SetActive(false);
-        questsMenu.SetActive(false);
+        StartCoroutine(ChangeMenu(pageTurn));
+    }
+
+    #region PauseMenu
+
+    public void TogglePauseState()
+    {
+        gameIsPaused = !gameIsPaused;
+
+        if (gameIsPaused)
+            Pause();
+        else Resume();
+    }
+
+
+    void Resume()
+    {
+        if(Time.timeScale < 1)
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 1, LerpPauseSpeed);
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        currentPage = 0;
+
         settingsMenu.SetActive(false);
         inventoryMenu.SetActive(false);
+        questsMenu.SetActive(false);
 
+        equipmentMenu.SetActive(true);
+        title.text = "Equipment";
+        leftOption.text = "Settings";
+        rightOption.text = "Quests";
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+        gameIsPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    IEnumerator ChangeMenu(int pageTurn)
+    {
         currentPage += pageTurn;
 
         if (currentPage < 0)
-            currentPage = numberOfPages-1;
+            currentPage = numberOfPages - 1;
 
-        if (currentPage > numberOfPages-1)
+        if (currentPage > numberOfPages - 1)
             currentPage = 0;
 
+        if (pageTurn == -1)
+            anim.SetTrigger("PanLeft");
+        else anim.SetTrigger("PanRight");
+
+        yield return new WaitForSecondsRealtime(0.8f);
+
+        settingsMenu.SetActive(false);
+        inventoryMenu.SetActive(false);
+        questsMenu.SetActive(false);
+        equipmentMenu.SetActive(false);
+
+        ShowMenu();
+    }
+
+    public void ShowMenu()
+    {
         switch (currentPage)
         {
             case 0:
@@ -72,43 +155,4 @@ public class PauseMenuManager : MonoBehaviour
                 break;
         }
     }
-
-    #region PauseMenu
-
-    public void TogglePauseState()
-    {
-        gameIsPaused = !gameIsPaused;
-
-        if (gameIsPaused)
-            Pause();
-        else Resume();
-    }
-
-
-    void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-    }
-
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        currentPage = 0;
-        UpdatePauseUI(0);
-    }
-
-    public void LoadMenu()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(0);
-        gameIsPaused = false;
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-    #endregion
 }
