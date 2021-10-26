35ebc53ce32a48b61856a8eddda00239889169d1
Thinkwanaut <39705721+Thinkwanaut@users.noreply.github.com>
2021-10-26 17:28:48 +0100

    # Implemented new camera system

--- Assets/Scripts/Entities/Player/CameraMove.cs
+++ Assets/Scripts/Entities/Player/CameraMove.cs
@@ -13,25 +13,65 @@ public class CameraMove : MonoBehaviour
     private bool XFollowing, ZFollowing;
     public float Yoffset = 10, Zoffset = 5;
 
-    public Vector3 Limits = new Vector3(15, 0, 5);   
+    public Vector3 Limits = new Vector3(15, 0, 5);
 
+    public float panSpeed = 0;
+    private float panLinger = 0;
+    private float panDuration = 0;
+    private float panTimer = 0;
+
+    private bool panning = false;
+    private Vector3 panTarget = new Vector3();
+    private Vector3 panStart = new Vector3();
+
+    private bool Bound => LeftBound != null && RightBound != null && UpBound != null && DownBound != null;
 
     private void Awake() //used to avoid reference errors
     {
+        if (Target == null) Target = FindObjectOfType<PlayerMovement_Jerzy>().transform;
         transform.position = new Vector3(Target.position.x, Yoffset, Zoffset);
     }
 
     void Update()
     {
-        XFollowing = RightBound.position.x - LeftBound.position.x > Limits.x * 2; //Can be done in awake if boundaries do not change mid-level
-        ZFollowing = UpBound.position.z - DownBound.position.z > Limits.z * 2;
+        if (!panning) FollowPlayerToLimits();
+        else Pan();
+    }
 
-        float Xmid = (RightBound.position.x + LeftBound.position.x) / 2;
-        float Zmid = (UpBound.position.z + DownBound.position.z) / 2;
+    private void FollowPlayerToLimits()
+    {
+        float newX = Target.position.x, newZ = Target.position.z;
 
-        float newX = XFollowing ? Mathf.Clamp(Target.position.x, LeftBound.position.x + Limits.x, RightBound.position.x - Limits.x) : Xmid;
-        float newZ = ZFollowing ? Mathf.Clamp(Target.position.z, DownBound.position.z + Limits.z, UpBound.position.z - Limits.z) : Zmid;
+        if (Bound)
+        {
+            XFollowing = RightBound.position.x - LeftBound.position.x > Limits.x * 2; //Can be done in awake if boundaries do not change mid-level
+            ZFollowing = UpBound.position.z - DownBound.position.z > Limits.z * 2;
 
-        transform.position = new Vector3(newX, Yoffset, (newZ - Zoffset));
-    }    
+            float Xmid = (RightBound.position.x + LeftBound.position.x) / 2;
+            float Zmid = (UpBound.position.z + DownBound.position.z) / 2;
+
+            newX = XFollowing ? Mathf.Clamp(Target.position.x, LeftBound.position.x + Limits.x, RightBound.position.x - Limits.x) : Xmid;
+            newZ = ZFollowing ? Mathf.Clamp(Target.position.z, DownBound.position.z + Limits.z, UpBound.position.z - Limits.z) : Zmid;
+        }
+        
+        transform.position = new Vector3(newX, Target.position.y + Yoffset, (newZ - Zoffset));
+    }
+
+    public void StartPan(Vector3 newPan, float linger)
+    {
+        panTarget = newPan;
+        panStart = transform.position;
+
+        panDuration = panStart.GetDistance(panTarget) / panSpeed;
+    }
+
+    private void Pan()
+    {
+        panTimer += Time.deltaTime;
+
+        if (panTimer < panDuration)
+            transform.position = Vector3.Lerp(panStart, panTarget, panTimer / panDuration);
+        else if (panTimer > panDuration + panLinger && panTimer < panDuration * 2 + panLinger)
+            transform.position = Vector3.Lerp(panTarget, panStart, (panTimer - panDuration - panLinger) / panDuration);
+    }
 }
