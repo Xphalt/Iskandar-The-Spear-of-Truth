93db74d3e088c0ec99c2edc2dda6e4cf60653374
Mattie Hilton <78374721+SnarfM@users.noreply.github.com>
2021-10-26 11:14:31 +0100

    Worked on Sound Library and Player organisation

--- Assets/Scripts/Audio/SoundLibrary.cs
+++ Assets/Scripts/Audio/SoundLibrary.cs
@@ -44,3 +44,86 @@ public class SoundLibrary : MonoBehaviour
         SoundLib.Add(SoundGroup.CombatStab, CombatStab);
     }
 }
+
+
+
+
+
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+
+/*
+ * Created by Mattie Hilton - 29/09/2021 
+ * Updated by Mattie Hilton - 26/10/2021
+ */
+
+//public class SoundLibrary : MonoBehaviour
+//{
+//    public enum SoundGroupParent
+//    {
+//        Player,
+//        Combat,
+//        Enemies
+//    }
+//    public enum SoundGroupChild
+//    {
+//        Breathing,
+//        Footstep,
+//        Damage,
+//        Drinking,
+//        Sword,
+//        BowArrow,
+//        Magic,
+//        Armour
+//    }
+//    public enum SoundGroupChildChild
+//    {
+//        NULL,
+//        Swing,
+//        Slash,
+//        Stab,
+//        ArrowSet,
+//        ArrowRelease,
+//        ArrowCollide,
+//        ArmourCollision
+//    }
+
+//    public Dictionary<SoundGroupParent, Dictionary<SoundGroupChild, Dictionary<SoundGroupChildChild, AudioClip[]>>> sfxClipLib =
+//        new Dictionary<SoundGroupParent, Dictionary<SoundGroupChild, Dictionary<SoundGroupChildChild, AudioClip[]>>>();
+
+//    public static SoundLibrary instance;
+
+//    private void Awake()
+//    {
+//        instance = this;
+//    }
+
+//    private void Start()
+//    {
+//        SFXClip[] clips = Resources.FindObjectsOfTypeAll(typeof(SFXClip)) as SFXClip[];
+
+//        foreach (var clip in clips)
+//        {
+//            Dictionary<SoundGroupChildChild, AudioClip[]> groupChildChild = new Dictionary<SoundGroupChildChild, AudioClip[]>
+//            {
+//                { clip.childChild, clip.audioClips }
+//            };
+
+//            Dictionary<SoundGroupChild, Dictionary<SoundGroupChildChild, AudioClip[]>> groupChild = new Dictionary<SoundGroupChild, Dictionary<SoundGroupChildChild, AudioClip[]>>
+//            {
+//                { clip.child, groupChildChild }
+//            };
+
+//            sfxClipLib.Add(clip.parent, groupChild);
+//        }
+//    }
+
+//    public AudioClip[] GetAudioClips(SoundGroupParent parent, SoundGroupChild child, SoundGroupChildChild childChild = SoundGroupChildChild.NULL)
+//    {
+//        AudioClip[] audioClips;
+//        Debug.Log(parent + " " + child + " " + childChild);
+//        audioClips = sfxClipLib[parent][child][childChild];
+//        return audioClips;
+//    }
+//}
