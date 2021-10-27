1f30354ae9188eada7afd5298bbc2ff482df2dbc
Mattie Hilton <78374721+SnarfM@users.noreply.github.com>
2021-10-27 13:19:52 +0100

    Disabled SoundLibrary

--- Assets/Scripts/Audio/SoundPlayer.cs
+++ Assets/Scripts/Audio/SoundPlayer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.Audio;
 
 /*
@@ -11,19 +12,28 @@ public class SoundPlayer : MonoBehaviour
     public AudioClip audioClip;
     internal AudioSource aSource;
     internal AudioMixer mixer;
+      
+    public void PlayAudio()
+    {
+        StartCoroutine(PlayAudioClip());
+    }
 
-    private void Start()
+    IEnumerator PlayAudioClip()
+    {
+        AddAudioSource();
+
+        aSource.clip = audioClip;
+        aSource.Play();
+
+        yield return new WaitForSeconds(aSource.clip.length);
+        Destroy(aSource);
+    }
+
+    private void AddAudioSource()
     {
         aSource = gameObject.AddComponent<AudioSource>();
         aSource.outputAudioMixerGroup = mixer.FindMatchingGroups("SFX")[0];
         aSource.loop = false;
         aSource.playOnAwake = false;
-    }            
-
-    //When calling, make sure the correct soundType is set beforehand, then call PlayAudio
-    public void PlayAudio()
-    {
-        aSource.clip = audioClip;
-        aSource.Play();
     }
 }
