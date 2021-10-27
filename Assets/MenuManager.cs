8158254e0722a47d7bf5a4d02161ba12b877d444
Mattie Hilton <78374721+SnarfM@users.noreply.github.com>
2021-10-27 15:29:08 +0100

    Started Working on Main Menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }  
+}
