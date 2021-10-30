c325e3288b906e18973d8271647725d71d3939e6
Fate Stredňáková Show <fateshow@gmail.com>
2021-10-30 15:49:49 +0100

    +Colour is grey if not in range

--- Assets/Scripts/UI/Interactable Obects/ToolTip.cs
+++ Assets/Scripts/UI/Interactable Obects/ToolTip.cs
@@ -9,28 +9,67 @@ using UnityEngine;
 public class ToolTip : MonoBehaviour
 {
     private SpriteRenderer SR;
-    private static int look = 0, use = 1, talk = 2; //Add list elements in Inspector in this order
-    public List<Sprite> toolTipImages = new List<Sprite>();
+    private Transform player;
+    //private static int look = 0, use = 1, talk = 2; //Add list elements in Inspector in this order
+    //public List<Sprite> toolTipImages = new List<Sprite>();
+    public Sprite lookSprite, useSprite, talkSprite;
+    public float nearRadius, farRadius;
 
     private void Awake()
     {
-        SR = gameObject.GetComponent<SpriteRenderer>();
+        SR = GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    private void Start() { SR.enabled = false; }
+    private void Start() 
+    { 
+        SR.enabled = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //Radius player can interact with object
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, nearRadius);
+        //Radius player can see object in distance
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, farRadius);
+    }
+
+    private void Update()
+    {
+        float toolTipRange = Vector3.Distance(player.position, transform.position);
+
+
+        if (toolTipRange <= farRadius && toolTipRange > nearRadius)
+        {
+            print("far range");
+            SR.color = Color.gray;
+        }
+        else if (toolTipRange <= nearRadius)
+        {
+            print("near range");
+            SR.color = Color.white;
+        }
+        else
+            print("not in range");
+    }
 
     public void SetImage(string type)
     {
         switch(type)
         {
             case "Look":
-                this.SR.sprite = toolTipImages[look];
+                //SR.sprite = toolTipImages[look];
+                SR.sprite = lookSprite;
                 break;
             case "Use":
-                this.SR.sprite = toolTipImages[use];
+                // SR.sprite = toolTipImages[use];
+                SR.sprite = useSprite;
                 break;
             case "Talk":
-                this.SR.sprite = toolTipImages[talk];
+                // SR.sprite = toolTipImages[talk];
+                SR.sprite = talkSprite;
                 break;
         }
     }
