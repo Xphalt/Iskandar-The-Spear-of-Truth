247b90c0e76db487281a427fed2285feac456d44
Salvatore <78374972+Adamska-01@users.noreply.github.com>
2021-10-31 14:53:46 +0000

    Added items

--- Assets/ScriptableObjects/UI/Sal_InventorySystem/ItemOBJECT.cs
+++ Assets/ScriptableObjects/UI/Sal_InventorySystem/ItemOBJECT.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ItemOBJECT : ItemObject_Sal
 {
     public float healingValue;
+    [Range(1, 100)] public float healPercentage;
 
     public float damage;
     public float explosionRadius;
