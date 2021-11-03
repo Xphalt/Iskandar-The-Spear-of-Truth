3bea92e80b4205129e5154fc467ad979d74bdaea
mattAlt <78375460+altMatt@users.noreply.github.com>
2021-11-03 12:28:45 +0000

    Spirit Enemy

---
+++ Assets/Scripts/Entities/Enemies/Spirit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spirit : EnemyBase
+{
+    
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+}
