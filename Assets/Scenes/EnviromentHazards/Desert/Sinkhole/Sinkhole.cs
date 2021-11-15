b8eb8d022fd089ae85c62528855e234051f2b9a4
rzed1 <45627650+rzed1@users.noreply.github.com>
2021-11-15 21:55:13 +0000

    Sinkhole material

---
+++ Assets/Scenes/EnviromentHazards/Desert/Sinkhole/Sinkhole.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Sinkhole : MonoBehaviour
+{
+    public Material material;
+
+    void Update()
+    {
+        if (material != null)
+        {
+            material.SetVector("_Position", new Vector2(transform.position.x,
+                transform.position.z));
+            material.SetVector("_Scale", new Vector2(transform.localScale.x,
+                transform.localScale.y));
+        }
+    }
+}
