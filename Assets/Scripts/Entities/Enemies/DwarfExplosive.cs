45db375cf0d6ccddf06429ebcbb0b4a85ca3c550
mattAlt <78375460+altMatt@users.noreply.github.com>
2021-11-16 21:18:55 +0000

    Updated dwarf and servant

--- Assets/Scripts/Entities/Enemies/DwarfExplosive.cs
+++ Assets/Scripts/Entities/Enemies/DwarfExplosive.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class DwarfExplosive : MonoBehaviour
 {
     public float Countdown;
+    public float explosionDamage;
+    public float explsionRadius;
+    public float knockbackSpeed;
+    public float duration;
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
         Countdown -= Time.deltaTime;
         if (Countdown <= 0)
         {
-            //explode
+            if ((player.transform.position - transform.position).magnitude <= explsionRadius)
+            {
+                player.GetComponent<PlayerMovement_Jerzy>().KnockBack(transform.position, knockbackSpeed, duration);
+                player.GetComponent<PlayerStats>().TakeDamage(explosionDamage, false);
+            }
+            Destroy(gameObject);
         }
     }
 }
