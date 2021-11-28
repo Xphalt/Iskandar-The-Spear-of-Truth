426a9abc36927224831d0a81eb88728a699e619b
Dominique Russell <30952031+whatever9999@users.noreply.github.com>
2021-11-28 19:08:35 +0000

    Loading screen with blackout triggers on level transition

--- Assets/Scenes/Test/FateTest/BlackoutScript.cs
+++ Assets/Scenes/Test/FateTest/BlackoutScript.cs
@@ -6,17 +6,21 @@ public class BlackoutScript : MonoBehaviour
 {
     private LoadScene loadScene;
     private Animator blackoutAnimator;
+
+    private int sceneIndex;
+
     private void Start()
     {
         loadScene = GetComponent<LoadScene>();
         blackoutAnimator = GetComponent<Animator>();
     }
-    public void FadeOutOfScene()
+    public void FadeOutOfScene(int index)
     {
+        sceneIndex = index;
         blackoutAnimator.SetTrigger("FadeBlack"); //Event within animation triggers GetLoadingScreen();
     }   
 
-    public void GetLoadingScreen(int sceneIndex)
+    public void GetLoadingScreen()
     {
         loadScene.Load(sceneIndex);
     }
