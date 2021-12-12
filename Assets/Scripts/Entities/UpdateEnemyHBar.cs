ba5039ea4c4550e3bf6f834bace46a4c14e29136
MegaMadMorgan <madassdog1@gmail.com>
2021-12-12 18:53:29 +0000

    + set up the basis for enemy health bars (prefabs)

---
+++ Assets/Scripts/Entities/UpdateEnemyHBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateEnemyHBar : MonoBehaviour
+{
+    public GameObject impactBar;
+    public GameObject healthBar;
+    public GameObject enemy;
+
+    float healthDifference;
+    public float impactTarget;
+    public float multiplier;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, 0, 0));
+
+        healthBar.GetComponent<Image>().fillAmount = enemy.GetComponent<EnemyStats>().health/enemy.GetComponent<EnemyStats>().MAX_HEALTH;
+        impactTarget = enemy.GetComponent<EnemyStats>().health;
+        //impactBar.GetComponent<Image>().fillAmount = Mathf.Lerp(impactBar.GetComponent<Image>().fillAmount, impactTarget, multiplier);
+        //multiplier *= 0.5f * Time.deltaTime;
+
+        if (impactBar.GetComponent<Image>().fillAmount > healthBar.GetComponent<Image>().fillAmount)
+        {
+            healthDifference = impactBar.GetComponent<Image>().fillAmount - healthBar.GetComponent<Image>().fillAmount;
+            healthDifference /= 20;
+            impactBar.GetComponent<Image>().fillAmount -= healthDifference;
+        }
+
+        //impactBar.GetComponent<Image>().fillAmount = 0;
+    }
+}
