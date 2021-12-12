dd3cb3d91af32bd2a4d836b91572932343035e6b
rzed1 <45627650+rzed1@users.noreply.github.com>
2021-12-12 15:05:23 +0000

    Enemies no longer take damage twice in 1 swing (take damage cooldown)

--- Assets/Scripts/Entities/Player/ThrowSword_Jerzy.cs
+++ Assets/Scripts/Entities/Player/ThrowSword_Jerzy.cs
@@ -154,6 +154,9 @@ public class ThrowSword_Jerzy : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // when the sword returns to the player
-        if (other.TryGetComponent(out EnemyStats statsInterface)) playerStats.DealDamage(statsInterface, swordDamage);
+        if (other.TryGetComponent(out EnemyStats statsInterface)) {
+            playerStats.DealDamage(statsInterface, swordDamage);
+        }
+
     }
 }
