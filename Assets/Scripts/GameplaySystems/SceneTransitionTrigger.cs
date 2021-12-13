133fc7540b7c0e950cc8a996c58d628bf76a4f08
Salvatore <78374972+Adamska-01@users.noreply.github.com>
2021-12-13 16:54:20 +0000

    items are picked up automatically when quitting the game

--- Assets/Scripts/GameplaySystems/SceneTransitionTrigger.cs
+++ Assets/Scripts/GameplaySystems/SceneTransitionTrigger.cs
@@ -11,19 +11,20 @@ public class SceneTransitionTrigger : MonoBehaviour
     {
         if (other.TryGetComponent(out PlayerStats stats))
         {
+            //Get items left over
             GroundItem[] groundObjs = FindObjectsOfType<GroundItem>();
             foreach (GroundItem item in groundObjs)
             {
                 if (item.itemobj.objType != ObjectType.Resource)
                 {
-                    if(item.itemobj.data.name.entryReference == "Health Drop") 
-                        Destroy(other.gameObject);    
+                    if (item.itemobj.data.name.entryReference == "Health Drop") 
+                        Destroy(item.gameObject); 
                     if (stats.equipment.GetSlots[(int)EquipSlot.ItemSlot].item.id == item.itemobj.data.id)
                     {
                         stats.equipment.GetSlots[(int)EquipSlot.ItemSlot].AddAmount(1);
                         Destroy(item.gameObject);
                     }
-                    else if (stats.inventory.AddItem(new Item(item.itemobj), 1)) 
+                    else if (stats.inventory.AddItem(new Item(item.itemobj), 1))
                         Destroy(item.gameObject);  //Only if the item is picked up 
                 }
                 else //It's a resource
@@ -32,11 +33,13 @@ public class SceneTransitionTrigger : MonoBehaviour
                     {
                         if (stats.inventory.FindItemOnInventory(item.itemobj.data) != null)
                         { }
-                        else if (stats.inventory.AddItem(new Item(item.itemobj), 1)) 
-                            Destroy(item.gameObject); 
-                    } 
-                    else if (stats.inventory.AddItem(new Item(item.itemobj), 1)) 
-                        Destroy(item.gameObject);  //Only if the item is picked up 
+                        else if (stats.inventory.AddItem(new Item(item.itemobj), 1))
+                            Destroy(item.gameObject);
+                    }
+                    else if ((((ResourceObject)(item.itemobj)).resourceType == ResourceType.Gems)) 
+                        Destroy(item.gameObject); 
+                    else if (stats.inventory.AddItem(new Item(item.itemobj), 1))
+                        Destroy(item.gameObject);  //Only if the item is picked up  
                 }
             } 
 
