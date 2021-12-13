c06ce751962f5de5800409a578c81fc3cd09ec0b
Mattie Hilton <78374721+SnarfM@users.noreply.github.com>
2021-12-13 13:37:45 +0000

    Bug #1212, #1215 Hack n Plan

--- Assets/SaveDataAssistant.cs
+++ Assets/SaveDataAssistant.cs
@@ -8,6 +8,7 @@ public class SaveDataAssistant : MonoBehaviour
     internal int currentSaveFileID = 0;
     public float SFXVol = -15;
     public float MusicVol = 0;
+    public float AmbienceVol = 0;
 
     private void Awake()
     {
